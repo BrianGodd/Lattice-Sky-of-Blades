5748c6c98869f9058a02c1423d78430a2c111315
NEKOOAO <60414oscar@gmail.com>
2025-12-18 21:04:07 +0800

    1-2

---
+++ Assets/PhaseMannger.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhaseMannger : MonoBehaviour
+{
+
+    [SerializeField] List< GameObject> Phasedisables;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void changeState(int i)
+    {
+        for (int j = 0; j < Phasedisables.Count; j++)
+        {
+            if (j == i)
+            {
+                Phasedisables[j].SetActive(true);
+            }
+            else
+            {
+                Phasedisables[j].SetActive(false);
+            }
+        }
+    }
+
+}
