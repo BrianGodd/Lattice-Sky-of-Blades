26db04e66cf480c522234a6ffea4a15117ad2366
NEKOOAO <60414oscar@gmail.com>
2025-12-20 23:05:13 +0800

    add spring

---
+++ Assets/spring.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spring : MonoBehaviour
+{
+
+    Vector3 forceV;
+    [SerializeField] Transform from, to;
+    [SerializeField] float force;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        forceV = (to.position - from.position).normalized * force;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            PlayerCharacter.Instance.AddForce(forceV);
+        }
+    }
+
+}
