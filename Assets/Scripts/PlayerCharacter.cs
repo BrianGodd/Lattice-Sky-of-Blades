7231656373e9af288935f39a73273edbfd853159
johnson777 <johnson1127.cs11@nycu.edu.tw>
2025-12-21 02:37:56 +0800

    try fix instance

--- Assets/Scripts/PlayerCharacter.cs
+++ Assets/Scripts/PlayerCharacter.cs
@@ -36,6 +36,7 @@ public struct CharacterInput
 }
 public class PlayerCharacter : MonoBehaviour, ICharacterController
 {
+    public static PlayerCharacter Instance { get; private set; }
     [SerializeField] private KinematicCharacterMotor motor;
     [SerializeField] private Transform root;
     [SerializeField] private Transform cameraTarget;
@@ -104,6 +105,12 @@ public class PlayerCharacter : MonoBehaviour, ICharacterController
     [SerializeField] private float minMagicSpeed;
     public void Initialize()
     {
+        if(Instance != null && Instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        Instance = this;
         motor.CharacterController = this;
     }
 
