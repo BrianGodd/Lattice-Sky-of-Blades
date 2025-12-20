9c9a21ce38ef5531dd1663a28191de0743bca958
NEKOOAO <60414oscar@gmail.com>
2025-12-20 22:12:10 +0800

    modfiy training stage

---
+++ Assets/TpBasic.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TpBasic : MonoBehaviour
+{
+    [SerializeField] float Tptime;
+    [SerializeField]GameObject tpto;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+       PlayerCharacter.Instance.SetTransform(tpto.transform.position);
+    }
+
+}
