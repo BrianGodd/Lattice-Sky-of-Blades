ba984946fdceb781a7244d853314a1225948a540
johnson777 <johnson1127.cs11@nycu.edu.tw>
2025-12-20 23:09:24 +0800

    cleanup

--- Assets/Scripts/RockMovingPlatform.cs
+++ Assets/Scripts/RockMovingPlatform.cs
@@ -10,11 +10,6 @@ public class RockMovingPlatform : MonoBehaviour, IMoverController
 {
     public PhysicsMover Mover;
     Vector3 v,start,target;
-
-
-
-    // public PlayableDirector Director;
-
     private Transform _transform;
 
     public void init(Vector3 speed,Vector3 start,Vector3 end)
@@ -34,24 +29,8 @@ public class RockMovingPlatform : MonoBehaviour, IMoverController
     // This is called every FixedUpdate by our PhysicsMover in order to tell it what pose it should go to
     public void UpdateMovement(out Vector3 goalPosition, out Quaternion goalRotation, float deltaTime)
     {
-        // // Remember pose before animation
-        // Vector3 _positionBeforeAnim = _transform.position;
-        // Quaternion _rotationBeforeAnim = _transform.rotation;
-
-        // // Update animation
-        // EvaluateAtTime(Time.time);
-
-        // // Set our platform's goal pose to the animation's
-        // goalPosition = _transform.position;
-        // goalRotation = _transform.rotation;
-
-        // // Reset the actual transform pose to where it was before evaluating. 
-        // // This is so that the real movement can be handled by the physics mover; not the animation
-        // _transform.position = _positionBeforeAnim;
-        // _transform.rotation = _rotationBeforeAnim;
         goalPosition = _transform.position + v * Time.deltaTime;
         goalRotation = _transform.rotation;
-
     }
 
     void Update()
@@ -62,9 +41,4 @@ public class RockMovingPlatform : MonoBehaviour, IMoverController
         }
     }
 
-    // public void EvaluateAtTime(double time)
-    // {
-    //     Director.time = time % Director.duration;
-    //     Director.Evaluate();
-    // }
 }
