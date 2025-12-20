a54fedee3424ba94fdbc76961c9a00842eaff11f
johnson777 <johnson1127.cs11@nycu.edu.tw>
2025-12-20 22:34:02 +0800

    fast fall no slide

--- Assets/Scripts/PlayerCharacter.cs
+++ Assets/Scripts/PlayerCharacter.cs
@@ -250,7 +250,7 @@ public class PlayerCharacter : MonoBehaviour, ICharacterController
                 Debug.Log("Fast Fall");
                 var currentVerticalSpeed = Vector3.Dot(currentVelocity, motor.CharacterUp);
                 currentVelocity += motor.CharacterUp * (-fastFallSpeed - currentVerticalSpeed);
-                // _requestedCrouch = false;
+                _requestedCrouch = false;
             }
 
             if(_requestedMovement.sqrMagnitude > 0f)
