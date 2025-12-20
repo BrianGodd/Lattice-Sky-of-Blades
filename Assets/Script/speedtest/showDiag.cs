e54c0af2486c4d24e9990fed9c56b566643bdb50
NEKOOAO <60414oscar@gmail.com>
2025-12-20 15:30:31 +0800

    add some little game

---
+++ Assets/Script/speedtest/showDiag.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class showDiag : MonoBehaviour
+{
+
+    [SerializeField] bool show = false;
+    [SerializeField] GameObject diag;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            diag.SetActive(show);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            diag.SetActive(show);
+        }
+    }
+}
